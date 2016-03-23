d9d35ccfa68067f0d220f7b329aa0bfe2c200777
Brian <blongdev@gmail.com>
2016-03-23 19:05:03 -0400

    empty sync adapter

--- app/src/main/java/com/blongdev/sift/BaseActivity.java
+++ app/src/main/java/com/blongdev/sift/BaseActivity.java
@@ -1,5 +1,8 @@
 package com.blongdev.sift;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Debug;
@@ -18,10 +21,15 @@ import android.widget.Toast;
 
 public class BaseActivity extends AppCompatActivity implements Reddit.OnRefreshCompleted {
 
+    public static final String ACCOUNT_TYPE = "blongdev.com";
+    public static final String ACCOUNT = "Sift";
+
     Reddit mReddit;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
     View mNavHeader;
+
+    Account mAccount;
 
 
     @Override
@@ -30,7 +38,9 @@ public class BaseActivity extends AppCompatActivity implements Reddit.OnRefreshC
 
         mReddit = Reddit.getInstance();
 
-        if(!mReddit.mRedditClient.isAuthenticated()) {
+        mAccount = CreateSyncAccount(this);
+
+        if (!mReddit.mRedditClient.isAuthenticated()) {
             mReddit.refreshKey(getApplicationContext(), this);
         }
     }
@@ -91,7 +101,7 @@ public class BaseActivity extends AppCompatActivity implements Reddit.OnRefreshC
         });
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close){
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
 
             @Override
             public void onDrawerClosed(View drawerView) {
@@ -127,10 +137,42 @@ public class BaseActivity extends AppCompatActivity implements Reddit.OnRefreshC
     }
 
     @Override
-    public void setContentView(@LayoutRes int layoutResID)
-    {
+    public void setContentView(@LayoutRes int layoutResID) {
         super.setContentView(layoutResID);
         onCreateDrawer();
+    }
+
+    /**
+     * Create a new dummy account for the sync adapter
+     *
+     * @param context The application context
+     */
+    public static Account CreateSyncAccount(Context context) {
+        // Create the account type and default account
+        Account newAccount = new Account(
+                ACCOUNT, ACCOUNT_TYPE);
+        // Get an instance of the Android account manager
+        AccountManager accountManager =
+                (AccountManager) context.getSystemService(
+                        ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
     }
 
 }
---
+++ app/src/main/java/com/blongdev/sift/SiftSyncAdapter.java
@@ -0,0 +1,59 @@
+package com.blongdev.sift;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+
+/**
+ * Created by Brian on 3/23/2016.
+ */
+public class SiftSyncAdapter extends AbstractThreadedSyncAdapter {
+    // Global variables
+    // Define a variable to contain a content resolver instance
+    ContentResolver mContentResolver;
+
+    /**
+     * Set up the sync adapter
+     */
+    public SiftSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+    }
+
+    /**
+     * Set up the sync adapter. This form of the
+     * constructor maintains compatibility with Android 3.0
+     * and later platform versions
+     */
+    public SiftSyncAdapter(
+            Context context,
+            boolean autoInitialize,
+            boolean allowParallelSyncs) {
+        super(context, autoInitialize, allowParallelSyncs);
+        /*
+         * If your app uses a content resolver, get an instance of it
+         * from the incoming Context
+         */
+        mContentResolver = context.getContentResolver();
+    }
+
+    @Override
+    public void onPerformSync(
+            Account account,
+            Bundle extras,
+            String authority,
+            ContentProviderClient provider,
+            SyncResult syncResult) {
+    /*
+     * Put the data transfer code here.
+     */
+    }
+}
